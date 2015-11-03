540f7a8525afdc6041ad723e55e806b037854f0b
Sean Caulfield <sean@yak.net>
2015-11-03 09:17:14 -0500

    skeletonification and some testing code

--- firmware/mark_iv/rapidshark_mk_iv.ino
+++ firmware/mark_iv/rapidshark_mk_iv.ino
@@ -22,6 +22,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include "vnh5019.h"
+
 #define SERIAL_DEBUG 1
 #define SERIAL_BAUD_RATE 9600
 
@@ -44,8 +46,8 @@
 #define PIN_DART_DETECT 2
 #define PIN_SW_PUSH     3
 #define PIN_SW_CLIP     4
-#define PIN_SW_FIRE     5
-#define PIN_SW_ACCEL    6
+#define PIN_SW_FIRE     6
+#define PIN_SW_ACCEL    5
 #define PIN_PUSH_A      7
 #define PIN_ACCEL_A     8
 #define PIN_ACCEL_PWM   9
@@ -61,21 +63,65 @@
 // Display parameters
 //
 
-#define DISP_TEXTSIZE 1
-#define DISP_COLOR    WHITE
-#define DISP_ADDR     0x3C
-#define DISP_MODE     SSD1306_SWITCHCAPVCC
+#define DISP_TEXT_SMALL 1
+#define DISP_TEXT_LARGE 2
+#define DISP_COLOR      WHITE
+#define DISP_ADDR       0x3C
+#define DISP_MODE       SSD1306_SWITCHCAPVCC
+
+//
+// Settings
+//
+
+#define MOTOR_ACCEL_SPEED 32
+#define MOTOR_PUSH_SPEED_SLOW 32
+#define MOTOR_PUSH_SPEED_FAST 32
 
 ////////////////////////////////////////////////////////////////////////
 // GLOBAL STATE VARIABLES
 ////////////////////////////////////////////////////////////////////////
 
 Adafruit_SSD1306 display(PIN_DISP_RST);
-//Adafruit_SSD1306 display = Adafruit_SSD1306();
+VNH5019 motor_accel = VNH5019(PIN_ACCEL_A, PIN_ACCEL_B, PIN_ACCEL_PWM);
+VNH5019 motor_push  = VNH5019(PIN_PUSH_A, PIN_PUSH_B, PIN_PUSH_PWM);
+
+volatile boolean trigAccel = false;
+volatile boolean trigFire = false;
+volatile boolean trigPush = false;
 
 ////////////////////////////////////////////////////////////////////////
 // "HALPING" FUNCTIONS
 ////////////////////////////////////////////////////////////////////////
+
+/*
+ * refreshDisplay
+ *
+ * Update status information on the display.
+ *
+ */
+void refreshDisplay() {
+
+  display.clearDisplay();
+
+  display.setCursor(40, 0);
+  display.setTextWrap(false);
+  display.setTextSize(4);
+  display.print("37");
+
+  display.setTextSize(1);
+  display.setCursor(0, 40);
+  display.print("ACC  ");
+  display.print(trigAccel ? "*" : " ");
+  display.setCursor(0, 48);
+  display.print("FIRE ");
+  display.print(trigFire ? "*" : " ");
+  display.setCursor(0, 56);
+  display.print("PUSH ");
+  display.print(trigPush ? "*" : " ");
+
+  display.display();
+
+}
 
 ////////////////////////////////////////////////////////////////////////
 // STARTUP CODE
@@ -110,13 +156,17 @@ void setup() {
   display.begin(DISP_MODE, DISP_ADDR);
   display.clearDisplay();
   display.setTextColor(DISP_COLOR);
-  display.setTextSize(DISP_TEXTSIZE);
-  display.println("I ARE BUTTS");
+  display.setTextSize(DISP_TEXT_LARGE);
+  display.println();
+  display.println("RapidShark");
+  display.println("  Mark IV");
   display.dim(false);
   display.display();
+  delay(1000);
 
-  // Testing
-  pinMode(13, OUTPUT);
+  // Initialize motor settings
+  motor_accel.setSpeed(MOTOR_ACCEL_SPEED);
+  motor_push.setSpeed(MOTOR_PUSH_SPEED_SLOW);
 
 }
 
@@ -126,17 +176,24 @@ void setup() {
 
 void loop() {
 
-  uint32_t now = millis();
-  DEBUG_PRINTLN(now);
+  trigAccel = (digitalRead(PIN_SW_ACCEL) == LOW) ? true : false;
+  trigFire = (digitalRead(PIN_SW_FIRE) == LOW) ? true : false;
+  trigPush = (digitalRead(PIN_SW_PUSH) == LOW) ? true : false;
 
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(now);
-  display.display();
+  if (trigAccel) {
+    motor_accel.go();
+  } else {
+    motor_accel.stop();
+  }
 
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
+  if (trigFire) {
+    motor_push.go();
+  } else {
+    motor_accel.brake();
+  }
+
+  refreshDisplay();
+
   delay(500);
 
 }
