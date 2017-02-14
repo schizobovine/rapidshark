7f59861ce3d4e8a1d72c30b19f4fe1d37a1c91b3
Sean Caulfield <sean@yak.net>
2017-02-13 22:49:09 -0500

    futzing with firmware

--- firmware/lipo_sensor_reader/lipo_sensor_reader.ino
+++ firmware/lipo_sensor_reader/lipo_sensor_reader.ino
@@ -5,11 +5,12 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <MicroView.h>
-#include <SoftwareSerial.h>
+#include <stdlib.h>
 
-#define LIPOMON_RX A0
-#define LIPOMON_TX A1
-#define LIPOMON_EN A2
+#define LIPOMON_EN  3
+#define LIPOMON_DAT 2
+#define LIPOMON_CLK 5
+#define BUTT A1
 
 #define LIPOMON_VCC 5.0
 #define LIPOMON_ADC 1024.0
@@ -38,10 +39,13 @@ const float VDIV[NUM_CELLS] = {
   (1.0 / 1024.0) * 5.0 * ((36000.0 + 10000.0) / 10000.0),
 };
 
-SoftwareSerial serial(LIPOMON_RX, LIPOMON_TX);
+//SoftwareSerial serial(LIPOMON_RX, LIPOMON_TX);
 
 uint8_t msb, lsb;
-uint16_t reading;
+uint16_t readings[NUM_CELLS];
+uint8_t buff[NUM_CELLS*2] = {
+  1, 2, 3, 4, 5, 6
+};
 float voltage;
 
 float calc_voltage(uint16_t adc_reading, uint8_t cell) {
@@ -50,10 +54,11 @@ float calc_voltage(uint16_t adc_reading, uint8_t cell) {
 
 void setup() {
   delay(200);
-  pinMode(LIPOMON_RX, INPUT);
-  pinMode(LIPOMON_TX, OUTPUT);
+  pinMode(LIPOMON_DAT, INPUT);
+  pinMode(LIPOMON_CLK, OUTPUT);
   pinMode(LIPOMON_EN, OUTPUT);
-  digitalWrite(LIPOMON_EN, HIGH);
+  pinMode(BUTT, INPUT_PULLUP);
+  digitalWrite(LIPOMON_EN, LOW);
   uView.begin();
   uView.clear(ALL);
   uView.clear(PAGE);
@@ -63,42 +68,74 @@ void setup() {
 void loop() {
 
   // Enable monitoring
-  digitalWrite(LIPOMON_EN, LOW);
+  //digitalWrite(LIPOMON_EN, HIGH);
+  if (digitalRead(BUTT) == LOW) {
 
-  // Connect over serial
-  serial.begin(SERIAL_BAUD);
+    // Wait a tick for it to crunch the numbers
+    uView.clear(ALL);
+    uView.setCursor(0, 0);
+    uView.print(F("READING"));
+    uView.display();
 
-  // Wait for data
-  while (!serial.available()) {
-    delay(10);
-  }
+    // Get data
+    for (uint8_t i=0; i<NUM_CELLS*2; i++) {
+      buff[i] = shiftIn(LIPOMON_DAT, LIPOMON_CLK, MSBFIRST);
+      //msb = shiftIn(LIPOMON_DAT, LIPOMON_CLK, MSBFIRST);
+      //delay(1);
+      //lsb = shiftIn(LIPOMON_DAT, LIPOMON_CLK, MSBFIRST);
+      //readings[i] = (msb << 8) | lsb;
+    }
 
-  // Clear display
-  uView.clear(PAGE);
+    // Disable monitoring
+    digitalWrite(LIPOMON_EN, LOW);
 
-  // Check for number of cells to match
-  char c = serial.read();
-  if (serial.read() == NUM_CELLS) {
-    for (uint8_t i=0; i<NUM_CELLS; i++) {
-      msb = serial.read();
-      lsb = serial.read();
-      reading = (msb << 8) + lsb;
-      voltage = calc_voltage(reading, i);
-      uView.print(i, DEC);
-      uView.print(" ");
-      uView.print(reading, HEX);
-      uView.print(" ");
-      uView.println(voltage, PREC);
+    // Print data to display
+    uView.clear(PAGE);
+    for (uint8_t i=0; i<NUM_CELLS*2; i++) {
+      //uView.print(readings[i], HEX);
+      //if (i % 2 == 0) {
+      //  uView.setCursor(0,  (i/2)*8);
+      //} else {
+      //  uView.setCursor(16, (i/2)*8);
+      //}
+      uView.setCursor(0, i*8);
+      uView.print(buff[i], BIN);
+      //uView.print(buff[2*i+1], HEX);
     }
     uView.display();
-  } else {
-    uView.print("bad # cells");
-    uView.display();
+
+    delay(1000);
+
   }
 
-  // Disable monitoring
-  digitalWrite(LIPOMON_EN, HIGH);
+  // Connect over serial
+  //serial.begin(SERIAL_BAUD);
 
-  delay(1000);
+  //// Wait for data
+  //while (!serial.available()) {
+  //  delay(10);
+  //}
+
+  //// Check for number of cells to match
+  //char c = serial.read();
+  //if (serial.read() == NUM_CELLS) {
+  //  for (uint8_t i=0; i<NUM_CELLS; i++) {
+  //    msb = serial.read();
+  //    lsb = serial.read();
+  //    reading = (msb << 8) + lsb;
+  //    voltage = calc_voltage(reading, i);
+  //    uView.print(i, DEC);
+  //    uView.print(" ");
+  //    uView.print(reading, HEX);
+  //    uView.print(" ");
+  //    uView.println(voltage, PREC);
+  //  }
+  //  uView.display();
+  //} else {
+  //  uView.print("bad # cells");
+  //  uView.display();
+  //}
+
+  //delay(1000);
 
 }
--- firmware/tiny_lipo_sensor/tiny_lipo_sensor.ino
+++ firmware/tiny_lipo_sensor/tiny_lipo_sensor.ino
@@ -6,7 +6,6 @@
  */
 
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
 ////////////////////////////////////////////////////////////////////////
 // PIN ASSIGNMENT
@@ -19,12 +18,11 @@ const uint8_t CELL_PINS[NUM_CELLS] = {
   A2  // cell 3
 };
 
-const uint8_t EN_DIV = 3;
-const uint8_t OUTPUT_EN = 4; // PA4 aka SCLK aka SCL
-const uint8_t SERIAL_RX = 5; // PA5 aka MISO
-const uint8_t SERIAL_TX = 6; // PA6 aka MOSI aka SDA
-
-#define SERIAL_BAUD 9600
+const uint8_t EN_DIV  = 3; // PA3
+const uint8_t PIN_CLK = 4; // PA4 aka SCLK aka SCL
+const uint8_t PIN_DAT = 5; // PA5 aka MISO aka DO
+const uint8_t PIN_EN  = 6; // PA6 aka MOSI aka DI aka SDA
+const uint8_t LED = 8; // PA6 aka MOSI aka DI aka SDA
 
 //
 // Voltage divider calculations
@@ -43,31 +41,176 @@ const uint8_t SERIAL_TX = 6; // PA6 aka MOSI aka SDA
 // GLOBAL STATE VARIABLES
 ////////////////////////////////////////////////////////////////////////
 
-// Pre-calculated array of voltage divider values
-const float VDIV[NUM_CELLS] = {
-  5.0 * (( 4900.0 + 10000.0) / 10000.0) / 1024.0,
-  5.0 * ((22100.0 + 10000.0) / 10000.0) / 1024.0,
-  5.0 * ((36000.0 + 10000.0) / 10000.0) / 1024.0,
+// Pre-calculated arrays of voltage divider multipliers, using some dirty fixed
+// point math to avoid floating point. These times the 
+const uint32_t VDIV[NUM_CELLS] = {
+  5000L * ( 4900L + 10000L) / 10000L,
+  5000L * (22100L + 10000L) / 10000L,
+  5000L * (36000L + 10000L) / 10000L
 };
 
-uint16_t readings[NUM_CELLS] = {
+// Number of bits of ADC resolution, used to divide reading after applying
+// voltage divider multiplier (...which is a bit of an oxymoron now that I read
+// it...).
+const uint8_t ADC_NUM_BITS = 10;
+
+// Readings in ADC ticks
+volatile uint16_t readings_raw[NUM_CELLS] = {
   0, 0, 0
 };
 
-float voltages[NUM_CELLS] = {
-  0.0, 0.0, 0.0
+// Do ADC -> millivolt conversion with enough precision before dropping back to
+// 16bit ints
+volatile uint32_t reading_temp = 0;
+
+// Readings in millivolts as 16-bit unsigned big-endian integers,
+// double-buffered to allow us to serve results while computing the next set.
+volatile uint16_t readings_buff[NUM_CELLS] = {
+  0, 0, 0
 };
 
-SoftwareSerial serial(SERIAL_RX, SERIAL_TX);
+// Size of the above buffer in BYTES
+//const uint8_t BUFF_SZ = sizeof(uint16_t) * NUM_CELLS;
+const uint8_t BUFF_SZ = 6;
+
+// Buffer position
+volatile uint8_t pos = 0;
+
+// In SPI transaction
+volatile boolean in_transaction = false;
+
+// Max position in buffer by byte (not number), so we can transfer the MSB and
+// LSB of the 16 bit results separately.
+//const uint8_t BUFF_POS_MAX = NUM_CELLS * 2;
+
+// Which of the two buffers is ready for reading by the master?
+//volatile uint8_t curr_buff = 0;
+
+// Where in that buffer the IRQ is reading from?
+//volatile uint8_t buff_read_pos = BUFF_POS_MAX;
+
+// Needed to prevent writer from swapping buffers while the other one is still
+// being read in a SPI transaction.
+//volatile boolean in_transaction = false;
+
+// Software UARt for sharting data out
+//SoftwareSerial serial(SERIAL_RX, SERIAL_TX);
 
 ////////////////////////////////////////////////////////////////////////
 // HELPER FUNCTIONS
 ////////////////////////////////////////////////////////////////////////
 
-void read_voltage(uint8_t cell_num) {
-  readings[cell_num] = analogRead(CELL_PINS[cell_num]);
-  voltages[cell_num] = readings[cell_num] * VDIV[cell_num];
-  return readings[cell_num];
+// Do a full cylce of enabling voltage dividers & reading the ADC.
+void do_read_voltages() {
+
+  // Enable dividers by flipping Q4 (N-MOS) gate HIGH, thereby flipping the
+  // gates for Q1, Q2 and Q3 (P-MOS) LOW, opening the connections to the cell
+  // terminals.
+  digitalWrite(EN_DIV, HIGH);
+
+  // Wait a tick! The ADC needs time to stabilize.
+  delay(10);
+
+  // Do the needful
+  readings_raw[0] = analogRead(CELL_PINS[0]);
+  readings_raw[1] = analogRead(CELL_PINS[1]);
+  readings_raw[2] = analogRead(CELL_PINS[2]);
+
+  // Disable dividers by doing the reverse of the whole dance above. In
+  // theory, you can take it back to tristate here, since the hardwired
+  // "default" should be the safe/power-saving version that keeps all the
+  // P-MOSFETs closed.
+  digitalWrite(EN_DIV, LOW);
+
+}
+
+// Conversion from raw ADC values to millivolts. Written to a double-buffer to
+// ensure consistent reads by the master.
+void do_calc_voltages() {
+  uint8_t i;
+
+  // Figure out which buffer we need to write to, whichever is not the one
+  // currently being read from. The switch between buffers is controlled by the
+  // SPI transaction process, to ensure consistent reads.
+  //other = (curr_buff + 1) % 2;
+
+  // Calculate voltages
+  for (i=0; i<NUM_CELLS; i++) {
+
+    // Multiply by (R1+R2)/R1 * 1000
+    //reading_temp = readings_raw[i] * VDIV[i];
+
+    // Normalize by ADC precision & store
+    //readings_buff[i] = reading_temp >> ADC_NUM_BITS;
+    readings_buff[i] = readings_raw[i];
+
+  }
+
+}
+
+// Busy wait based writing of buffers on bus. Helps that we can ignore the data
+// from the master.
+void put_buffers() {
+
+  // Pre-put first byte
+  USIDR = ((uint8_t*)readings_buff)[0];
+
+  // Reset buff position
+  pos = 1;
+
+  // Put in transaction
+  in_transaction = true;
+
+  // Enable overflow interrupt
+  USICR |= (1<<USIOIE);
+
+  // Wait for transmission to finish
+  while (in_transaction)
+    ;
+
+  // Disable overflow interrupt
+  USICR &= ~(1<<USIOIE);
+
+
+}
+
+////////////////////////////////////////////////////////////////////////
+// INTERRUPT HANDLERS
+////////////////////////////////////////////////////////////////////////
+
+// Interrupt handler for USI overflow vector, which gets called when slave
+// transmission of a byte is complete. When this is tripped, we move the next
+// byte into the transmission register and (optionally, we're not doing this)
+// read out whatever the master sent us. Additionally, we advance buffer
+// position, optionally swapping buffers if the other is ready.
+ISR(USI_OVF_vect) {
+
+  // Clear USI counter overflow
+  USISR |= (1<<USIOIF);
+
+  // If not in a transaction, what are we doing here?!?
+  if (!in_transaction)
+    return;
+
+  // Put next byte on bus
+  if (pos < BUFF_SZ) {
+    USIDR = ((uint8_t*)readings_buff)[pos];
+    pos += 1;
+  }
+  
+  // If done with transaction, unblock main
+  if (pos == BUFF_SZ) {
+    in_transaction = false;
+  }
+  // Figure out which byte is next for reading
+  //if (buff_read_pos < BUFF_MAX_POS) {
+
+  // Transmission complete, set transaction flag off and repoint buffer at
+  // start of array.
+  //} else {
+  //  buff_read_pos = 0;
+  //  in_transaction = false;
+  //}
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -75,12 +218,30 @@ void read_voltage(uint8_t cell_num) {
 ////////////////////////////////////////////////////////////////////////
 
 void setup() {
-  delay(200);
+
+  //delay(200);
+
+  // Initialize divider to be default off
   pinMode(EN_DIV, OUTPUT);
   digitalWrite(EN_DIV, LOW);
-  pinMode(OUTPUT_EN, INPUT_PULLUP);
-  //pinMode(SERIAL_RX, INPUT);
-  //pinMode(SERIAL_TX, OUTPUT);
+
+  // Setup pins for "SPI" output
+  pinMode(PIN_CLK, INPUT_PULLUP);
+  pinMode(PIN_DAT, OUTPUT);
+  pinMode(PIN_EN,  INPUT_PULLUP);
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
+  // Setup USI in 3-wire mode (though we'll be ignoring the input from the
+  // master, since that's our enable line), with external clocking (slave mode)
+  // on positive edges.
+  USICR = (1<<USIWM0) | (1<<USICS1);
+
+  readings_buff[0] = 1;
+  readings_buff[1] = 2;
+  readings_buff[2] = 3;
+
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -90,26 +251,16 @@ void setup() {
 void loop() {
 
   // Check chip select pin to see if we should do jack and/or shit
-  if (digitalRead(OUTPUT_EN) == LOW) {
+  //if (digitalRead(PIN_EN) == LOW) {
 
-    // Enable dividers by flipping Q4 (N-MOS) gate HIGH, thereby flipping the
-    // gates for Q1, Q2 and Q3 (P-MOS) LOW, opening the connections to the cell
-    // terminals.
-    digitalWrite(EN_DIV, HIGH);
-
-    // Wait a tick! The ADC needs time to stabilize.
-    delay(10);
-
-    // Do the needful
-    for (uint8_t i=0; i<NUM_CELLS; i++) {
-      readings[i] = analogRead(CELL_PINS[i]);
-    }
-
-    // Disable dividers by doing the reverse of the whole dance above. In
-    // theory, you can take it back to tristate here, since the hardwired
-    // "default" should be the safe/power-saving version that keeps all the
-    // P-MOSFETs closed.
-    digitalWrite(EN_DIV, LOW);
+    digitalWrite(LED, HIGH);
+    do_read_voltages();
+    do_calc_voltages();
+    //readings_buff[0] += 3;
+    //readings_buff[1] += 3;
+    //readings_buff[2] += 3;
+    put_buffers();
+    digitalWrite(LED, LOW);
 
     // Voltage calculations
     //for (uint8_t i=0; i<NUM_CELLS; i++) {
@@ -124,22 +275,22 @@ void loop() {
     // data.
     //
 
-    serial.begin(SERIAL_BAUD);
-    serial.write((uint8_t)NUM_CELLS);
-    for (uint8_t i=0; i<NUM_CELLS; i++) {
-      serial.write((uint8_t)(i+1)); // Cell num
-      serial.write((uint8_t)((readings[i] >> 0) & 0xFF));
-      serial.write((uint8_t)((readings[i] >> 8) & 0xFF));
-    }
-    serial.write((uint8_t)0xFF);
-    serial.flush();
-    serial.end();
+    //serial.begin(SERIAL_BAUD);
+    //serial.write((uint8_t)NUM_CELLS);
+    //for (uint8_t i=0; i<NUM_CELLS; i++) {
+    //  serial.write((uint8_t)(i+1)); // Cell num
+    //  serial.write((uint8_t)((readings[i] >> 0) & 0xFF));
+    //  serial.write((uint8_t)((readings[i] >> 8) & 0xFF));
+    //}
+    //serial.write((uint8_t)0xFF);
+    //serial.flush();
+    //serial.end();
 
-    // Because I'm feeling sassy. And hate battery life (despite desiging this
-    // whole circuit around it (don't judge my hypocracy! :P)).
-    delay(100);
+    //// Because I'm feeling sassy. And hate battery life (despite desiging this
+    //// whole circuit around it (don't judge my hypocracy! :P)).
+    //delay(100);
 
-  }
+  //}
 
 }
 
